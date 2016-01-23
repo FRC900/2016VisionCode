e302910752b3948998478f118983a9d6a3481b20
Team 900 Progammers <programmers@team900.org>
2016-01-23 16:43:48 -0500

    Integrated persistence library to find peaks in histogram data

--- fovis/fovis_testing.cpp
+++ fovis/fovis_testing.cpp
@@ -8,14 +8,16 @@
 
 #include <iostream>
 #include <string>
-#include <fovis/fovis.hpp>
+#include <fovis.hpp>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 
+#include "persistence1d.hpp"
 #include "zedin.hpp"
+
 
 using namespace std;
 using namespace cv;
@@ -36,18 +38,22 @@ string isometryToString(const Eigen::Isometry3d& m)
   return std::string(result);
 }
 
-double avgOfMat(const Mat& img, const Mat& mask) { //this averages mats without counting NaN
-	float *input = (float*)(img.data);
+double avgOfMat(Mat &img, Mat &mask) { //this averages mats without counting NaN
+	CV_Assert(img.depth() == CV_32F); //must be of type used for depth
+	float* ptr_img;
+	float* ptr_mask;
 	double sum = 0;
-	float datum;
 	for(int j = 0;j < img.rows;j++){
+
+	    ptr_img = img.ptr<float>(j);
+	    ptr_mask = mask.ptr<float>(j);
+
 	    for(int i = 0;i < img.cols;i++){
-		datum = input[img.cols * j + i ];
-		if(datum != NAN && mask[mask.cols * j + i] == 255) //if the data is not 0 and mask is true
-		    sum = sum + datum;
+		if(ptr_img[i] != NAN && ptr_mask[i] == 255) //if the data is not NAN and mask is true
+		    sum = sum + ptr_img[i];
 	    }
 	}
-	return sum / (float)(img.rows * img.cols)
+	return sum / (float)(img.rows * img.cols);
 }
 
 int main(int argc, char **argv) {
@@ -115,7 +121,8 @@ int main(int argc, char **argv) {
   Point  histMaxValLoc;
   fovis::DepthImage depthSource(rgb_params, cap->width, cap->height);
   clock_t startTime;
-  vector<double> ranges;
+  vector<float> peaks;
+  vector<float> histVec;
   while(1)
     {
     startTime = clock();
@@ -149,39 +156,31 @@ int main(int argc, char **argv) {
     calcHist(&depthFrame,1,0,Mat(),histMat,1,&numHistBins,&histRange, uniform, accumulate); //create a histogram, depth on the x and num pixels on the y
     int hist_img_width = 512; int hist_img_height = 400;
     int hist_bin_img_width = cvRound( (double)hist_img_width / numHistBins);
-    //cout << "histogram bin width: " << ((histRange_arr[1] - histRange_arr[0]) / (float)numHistBins) * 100.0 << " cm" << endl;
     float bin_width_m = (histRange_arr[1] - histRange_arr[0]) / (float)numHistBins;
 
     histMat.at<float>(0) = 0; //first bin includes NaN and gets very large so ignore them
-    meanStdDev(histMat,hist_stddev_mat,hist_mean_mat); //calculate the mean and stddev of the histogram
-    minMaxLoc(histMat,NULL,&histMaxVal,NULL,&histMaxValLoc); //calculate peak in the histogram
+    //meanStdDev(histMat,hist_stddev_mat,hist_mean_mat); //calculate the mean and stddev of the histogram
+    //minMaxLoc(histMat,NULL,&histMaxVal,NULL,&histMaxValLoc); //calculate peak in the histogram
 
-    //cout << "Peak depth location: " << histMaxValLoc.y * bin_width_m << endl;
-    ranges.clear();
-    ranges.push_back((float)histMaxValLoc.y - (rangeRangeInt / 100.0));
-    ranges.push_back((float)histMaxValLoc.y + (rangeRangeInt / 100.0));
-    cout << "Range is: " << ranges[0] * bin_width_m << " " << ranges[1] * bin_width_m << endl;
+    peaks.clear();
+    histVec.clear();
+    for(int i = 0; i < histMat.cols; i++)
+	histVec.push_back(histMat.at<float>(i));
 
-    #if FALSE
-    double range_threshhold = hist_stddev_mat.at<double>(0) * stddev_weight + hist_mean_mat.at<double>(0); //if a point is above this threshhold we consider it a peak
-    for(int i = 0; i < histMat.rows * histMat.cols; i++) { //this creates ranges of depths that are peaks
-	if(histMat.at<int>(i) > range_threshhold) //if the point is on a range
-	    if(!(histMat.at<int>(i-1) > range_threshhold)) //if the previous point is not on a range
-		ranges.push_back(i);
-	    else if(!(histMat.at<int>(i+1) > range_threshhold)) //or if the next point is not on a range
-		ranges.push_back(i);
-	}
+    p1d::Persistence1D p; //use a library called Persistence to find all maximums
+    p.RunPersistence(histVec);
+    vector< p1d::TPairedExtrema > Extrema;
+    p.GetPairedExtrema(Extrema, 10);
+    //Print all found pairs - pairs are sorted ascending wrt. persistence.
+    for(vector< p1d::TPairedExtrema >::iterator it = Extrema.begin(); it != Extrema.end(); it++)
+    {
+        cout << "Persistence: " << (*it).Persistence
+             << " minimum index: " << (*it).MinIndex
+             << " maximum index: " << (*it).MaxIndex
+             << std::endl;
+	peaks.push_back((*it).MaxIndex);
 
-    for(int i = 0; i < ranges.size(); i+=2) { //range * bin width = depth
-	cout << "Range " << i << " : " << ranges[i] * bin_width_m << "-" << ranges[i+1] * bin_width_m << endl;	
-	}
-    //cout << "Number of ranges: " << ranges.size() << endl;
-    //cout << "Last range in m: " << ranges[ranges.size() - 2] * bin_width_m << " to " << ranges[ranges.size() - 1] * bin_width_m << endl;
-    for(int i = 0; i < histMat.rows * histMat.cols; i++) {
-	if(histMat.at<int>(i) > range_threshhold) //if not a peak set to zero, so that we can only see peaks
-	    histMat.at<int>(i) = 0;
-	}
-    #endif
+    }
 
     Mat histImage(hist_img_height,hist_img_width, CV_8UC1, Scalar(0,0,0));
     normalize(histMat, histMat, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
@@ -198,22 +197,20 @@ int main(int argc, char **argv) {
 
     frame.copyTo(displayFrame); //make a color copy for display purposes
     cvtColor(displayFrame,displayFrame,CV_BGR2HSV); //convert to hsv
-    inRange(depthFrame,Scalar(ranges[ranges.size() - 2] * bin_width_m), Scalar(ranges[ranges.size() - 1] * bin_width_m),depthMask);
-    //meanStdDev(depthFrame,depth_mean_mat_1, depth_stddev_mat_1,depthMask); //average of depth of majority of frame
-    double depth_mean_1 = avgOfMat(depthFrame,depthMask);
-    bitwise_not(depthMask,depthMask_inv);
-    double depth_mean_2 = avgOfMat(depthFrame,depthMask_inv);
-    //meanStdDev(depthFrame,depth_mean_mat_2, depth_stddev_mat_2,depthMask_inv); //average of depth of minority of frame
+    inRange(depthFrame,Scalar((peaks[peaks.size() - 1] * bin_width_m) - (rangeRangeInt / 1000.0)), Scalar((peaks[peaks.size() - 1] * bin_width_m) + (rangeRangeInt / 1000.0)),depthMask);
+    double depth_mean_1 = avgOfMat(depthFrame,depthMask); //find the average of the "background"
+    bitwise_not(depthMask,depthMask_inv); //invert the mat
+    double depth_mean_2 = avgOfMat(depthFrame,depthMask_inv); //find the average of the "foreground"
     cout << "Mean of majority: " << depth_mean_1 << endl;
     cout << "Mean of minority: " << depth_mean_2 << endl;
     cout << "Mean of chosen: ";
-    if(depth_mean_2 > depth_mean_1 { //if the minority of the image is the farther one
-	bitwise_not(depthMask,depthMask); //invert the mask
+    if(depth_mean_2 > depth_mean_1) { //if the foreground of the image is the farther one than
+	bitwise_not(depthMask,depthMask); //invert the mask because we are obviously wrong about that
 	cout << depth_mean_2 << endl;
 	} else {
 	cout << depth_mean_1 << endl;
 	}
-    add(displayFrame,Scalar(140,0,0), displayFrame, depthMask);
+    add(displayFrame,Scalar(140,0,0), displayFrame, depthMask); //tint the background so we can see which parts are background for display
     cvtColor(displayFrame,displayFrame,CV_HSV2BGR);
     imshow("frame",displayFrame);
     imshow("background mask", depthMask);
