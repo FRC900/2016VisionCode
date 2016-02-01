43fd010076cab0a5a93928c945893f4798f4e7f0
Ben Decker <decker16b@ncssm.edu>
2016-02-01 18:14:07 -0500

    Beautified code

--- goal-detection/GoalDetector.cpp
+++ goal-detection/GoalDetector.cpp
@@ -3,144 +3,156 @@
 using namespace std;
 using namespace cv;
 
-GoalDetector::GoalDetector() {
-
-_goal_shape_contour.push_back(Point(0,0));
-_goal_shape_contour.push_back(Point(0,609.6));
-_goal_shape_contour.push_back(Point(50.8,609.6));
-_goal_shape_contour.push_back(Point(50.8,50.8)); //describes vision target shape
-_goal_shape_contour.push_back(Point(762,50.8)); //in mm because m doesn't work
-_goal_shape_contour.push_back(Point(762,609.6));
-_goal_shape_contour.push_back(Point(812.8,609.6));
-_goal_shape_contour.push_back(Point(812.8,0));
-
+GoalDetector::GoalDetector()
+{
+    _goal_shape_contour.push_back(Point(0, 0));
+    _goal_shape_contour.push_back(Point(0, 609.6));
+    _goal_shape_contour.push_back(Point(50.8, 609.6));
+    _goal_shape_contour.push_back(Point(50.8, 50.8)); //describes vision target shape
+    _goal_shape_contour.push_back(Point(762, 50.8));  //in mm because m doesn't work
+    _goal_shape_contour.push_back(Point(762, 609.6));
+    _goal_shape_contour.push_back(Point(812.8, 609.6));
+    _goal_shape_contour.push_back(Point(812.8, 0));
 }
 
-bool GoalDetector::processFrame(Mat &image, Mat &depth) {
 
-Mat hsv_image, threshold_image;
-Mat contour_mask(image.rows, image.cols, CV_8UC1, Scalar(0));
-vector< vector<Point> > contours;
-vector<Vec4i> hierarchy;
-Point2f center_of_area;
-Rect contour_rect;
+bool GoalDetector::processFrame(Mat& image, Mat& depth)
+{
+    Mat hsv_image, threshold_image;
+    Mat contour_mask(image.rows, image.cols, CV_8UC1, Scalar(0));
 
-cvtColor(image, hsv_image, COLOR_BGR2HSV); //thresholds the h,s,v values of the image to look for green
-generateThreshold(image, threshold_image, _hue_min, _hue_max, _sat_min, _sat_max, _val_min, _val_max);
+    vector<vector<Point> > contours;
+    vector<Vec4i>          hierarchy;
+    Point2f                center_of_area;
+    Rect contour_rect;
 
-findContours( threshold_image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    cvtColor(image, hsv_image, COLOR_BGR2HSV); //thresholds the h,s,v values of the image to look for green
+    generateThreshold(image, threshold_image, _hue_min, _hue_max, _sat_min, _sat_max, _val_min, _val_max);
+
+    findContours(threshold_image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 //find contours in the thresholded image
 
-float max_contour_area = 0;
+    float max_contour_area = 0;
 
-for (int i = 0; i < contours.size(); i++) {
-	Moments mu = moments( contours[i], false ); //get the center of area of the contour
-	center_of_area = Point2f( mu.m10/mu.m00 , mu.m01/mu.m00 ); //this could be used possibly for comparison to the center of area of the shape
+    for (int i = 0; i < contours.size(); i++)
+    {
+        Moments mu = moments(contours[i], false);                   //get the center of area of the contour
+        center_of_area = Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00); //this could be used possibly for comparison to the center of area of the shape
 
-	contour_rect = boundingRect(contours[i]); //bounding box of the target
+        contour_rect = boundingRect(contours[i]);                   //bounding box of the target
 
-	contour_mask.setTo(Scalar(0));
+        contour_mask.setTo(Scalar(0));
 
-	drawContours(contour_mask,contours,i,Scalar(255),CV_FILLED); //create a mask on the contour
-
-
-	float depth_z_min = minOfMat(depth,contour_mask,countPixel); //get the minimum and maximum depth values in the contour
-	float depth_z_max = minOfMat(depth,contour_mask,countPixel,true); //actually does some averaging
-
-	float h_dist_with_min = sqrt( (depth_z_min*depth_z_min) - (_goal_height*_goal_height) ); //uses pythagorean theorem to determine horizontal distance to goal using minimum
-	float h_dist_with_max = sqrt( (depth_z_max*depth_z_max) - ((_goal_height+contour_rect.height/1000.0)*(_goal_height+contour_rect.height/1000.0))); //this one uses maximum
-	float h_dist = (h_dist_with_max + h_dist_with_min) / 2.0; //average of the two is more accurate
-
-	float goal_to_center_px = ((float)contour_rect.tl().x + ((float)contour_rect.width / 2.0)) - ((float)image.cols / 2.0); //number of pixels from center of contour to center of image (e.g. how far off center it is)
-	float goal_to_center_deg = _camera_hfov * (goal_to_center_px / (float)image.cols); //converts to angle using the field of view
-	
-	if(contourArea(contours[i]) > max_contour_area) { //if this contour is the biggest one pick it
-		_dist_to_goal = h_dist; 
-		_angle_to_goal = goal_to_center_deg * (180.0 / M_PI);
-		max_contour_area = contourArea(contours[i]); //set variables		
-		}
-	}
-
-}
+        drawContours(contour_mask, contours, i, Scalar(255), CV_FILLED); //create a mask on the contour
 
 
+        float depth_z_min = minOfMat(depth, contour_mask, countPixel);                                                                                               //get the minimum and maximum depth values in the contour
+        float depth_z_max = minOfMat(depth, contour_mask, countPixel, true);                                                                                         //actually does some averaging
 
-float GoalDetector::minOfMat(Mat &img, Mat &mask, bool (*f)(float), bool max, int range) { //this actually gets min or max
+        float h_dist_with_min = sqrt((depth_z_min * depth_z_min) - (_goal_height * _goal_height));                                                                   //uses pythagorean theorem to determine horizontal distance to goal using minimum
+        float h_dist_with_max = sqrt((depth_z_max * depth_z_max) - ((_goal_height + contour_rect.height / 1000.0) * (_goal_height + contour_rect.height / 1000.0))); //this one uses maximum
+        float h_dist          = (h_dist_with_max + h_dist_with_min) / 2.0;                                                                                           //average of the two is more accurate
 
-CV_Assert(img.depth() == CV_32F); //must be of type used for depth
-CV_Assert(mask.depth() == CV_8UC1);
-float* ptr_img;
-uchar* ptr_mask;
-double min = 100000000;
-if(max)
-  min = 0;
-int minLoc_x;
-int minLoc_y;
+        float goal_to_center_px  = ((float)contour_rect.tl().x + ((float)contour_rect.width / 2.0)) - ((float)image.cols / 2.0);                                     //number of pixels from center of contour to center of image (e.g. how far off center it is)
+        float goal_to_center_deg = _camera_hfov * (goal_to_center_px / (float)image.cols);                                                                           //converts to angle using the field of view
 
-for(int j = 0;j < img.rows;j++){ //for each row
-
-    ptr_img = img.ptr<float>(j);
-    ptr_mask = mask.ptr<uchar>(j);
-
-    for(int i = 0;i < img.cols;i++){ //for each pixel in row
-	if(f(ptr_img[i]) && ptr_mask[i] == 255) {
-	    if(max) {
-	       if(ptr_img[i] > min)
-		   min = ptr_img[i];
-		   minLoc_x = i;
-		   minLoc_y = j;
-	    } else {
-	       if(ptr_img[i] < min)
-		   min = ptr_img[i];
-		   minLoc_x = i;
-		   minLoc_y = j;
-		}
-	}
+        if (contourArea(contours[i]) > max_contour_area)                                                                                                             //if this contour is the biggest one pick it
+        {
+            _dist_to_goal    = h_dist;
+            _angle_to_goal   = goal_to_center_deg * (180.0 / M_PI);
+            max_contour_area = contourArea(contours[i]);     //set variables
+        }
     }
 }
 
-int sum = 0;
-int numPix = 0;
-for(int j = (minLoc_x - range); j < (minLoc_x + range); j++) {
-	for(int i = (minLoc_y - range); i < (minLoc_y + range); i++) {
 
-		if( 0 < i && i < img.cols && 0 < j && j < img.rows && f(img.at<float>(i,j)) && mask.at<uchar>(i,j) == 255) {
-			sum = sum + img.at<float>(i,j);
-			numPix++;
-			}
-
-		}	
-	}
-
-return sum / (numPix * 1000.0);
-
-}
-
-
-
-void GoalDetector::generateThreshold(const Mat &ImageIn, Mat &ImageOut, int H_MIN, int H_MAX, int S_MIN, int S_MAX, int V_MIN, int V_MAX) {
-
-Mat ThresholdLocalImage;
-vector<Mat> SplitImage;
-Mat SplitImageLE;
-Mat SplitImageGE;
-
-cvtColor(ImageIn, ThresholdLocalImage, CV_BGR2HSV, 0);
-split(ThresholdLocalImage, SplitImage);
-int max[3] = {H_MAX, S_MAX, V_MAX};
-int min[3] = {H_MIN, S_MIN, V_MIN};
-for (size_t i = 0; i < SplitImage.size(); i++)
+float GoalDetector::minOfMat(Mat& img, Mat& mask, bool (*f)(float), bool max, int range) //this actually gets min or max
 {
-compare(SplitImage[i], min[i], SplitImageGE, cv::CMP_GE);
-compare(SplitImage[i], max[i], SplitImageLE, cv::CMP_LE);
-bitwise_and(SplitImageGE, SplitImageLE, SplitImage[i]);
+    CV_Assert(img.depth() == CV_32F);                                                    //must be of type used for depth
+    CV_Assert(mask.depth() == CV_8UC1);
+    float  *ptr_img;
+    uchar  *ptr_mask;
+    double min = 100000000;
+    if (max)
+    {
+        min = 0;
+    }
+    int minLoc_x;
+    int minLoc_y;
+
+    for (int j = 0; j < img.rows; j++) //for each row
+
+    {
+        ptr_img  = img.ptr<float>(j);
+        ptr_mask = mask.ptr<uchar>(j);
+
+        for (int i = 0; i < img.cols; i++) //for each pixel in row
+        {
+            if (f(ptr_img[i]) && (ptr_mask[i] == 255))
+            {
+                if (max)
+                {
+                    if (ptr_img[i] > min)
+                    {
+                        min = ptr_img[i];
+                    }
+                    minLoc_x = i;
+                    minLoc_y = j;
+                }
+                else
+                {
+                    if (ptr_img[i] < min)
+                    {
+                        min = ptr_img[i];
+                    }
+                    minLoc_x = i;
+                    minLoc_y = j;
+                }
+            }
+        }
+    }
+
+    int sum    = 0;
+    int numPix = 0;
+    for (int j = (minLoc_x - range); j < (minLoc_x + range); j++)
+    {
+        for (int i = (minLoc_y - range); i < (minLoc_y + range); i++)
+        {
+            if ((0 < i) && (i < img.cols) && (0 < j) && (j < img.rows) && f(img.at<float>(i, j)) && (mask.at<uchar>(i, j) == 255))
+            {
+                sum = sum + img.at<float>(i, j);
+                numPix++;
+            }
+        }
+    }
+
+    return sum / (numPix * 1000.0);
 }
-bitwise_and(SplitImage[0], SplitImage[1], ImageOut);
-bitwise_and(SplitImage[2], ImageOut, ImageOut);
 
-Mat erodeElement (getStructuringElement( MORPH_RECT,Size(3,3)));
-Mat dilateElement(getStructuringElement( MORPH_ELLIPSE,Size(2,2)));
-erode(ImageOut, ImageOut, erodeElement, Point(-1,-1), 2);
-dilate(ImageOut, ImageOut, dilateElement, Point(-1,-1), 2);
 
+void GoalDetector::generateThreshold(const Mat& ImageIn, Mat& ImageOut, int H_MIN, int H_MAX, int S_MIN, int S_MAX, int V_MIN, int V_MAX)
+{
+    Mat ThresholdLocalImage;
+
+    vector<Mat> SplitImage;
+    Mat         SplitImageLE;
+    Mat         SplitImageGE;
+
+    cvtColor(ImageIn, ThresholdLocalImage, CV_BGR2HSV, 0);
+    split(ThresholdLocalImage, SplitImage);
+    int max[3] = { H_MAX, S_MAX, V_MAX };
+    int min[3] = { H_MIN, S_MIN, V_MIN };
+    for (size_t i = 0; i < SplitImage.size(); i++)
+    {
+        compare(SplitImage[i], min[i], SplitImageGE, cv::CMP_GE);
+        compare(SplitImage[i], max[i], SplitImageLE, cv::CMP_LE);
+        bitwise_and(SplitImageGE, SplitImageLE, SplitImage[i]);
+    }
+    bitwise_and(SplitImage[0], SplitImage[1], ImageOut);
+    bitwise_and(SplitImage[2], ImageOut, ImageOut);
+
+    Mat erodeElement(getStructuringElement(MORPH_RECT, Size(3, 3)));
+    Mat dilateElement(getStructuringElement(MORPH_ELLIPSE, Size(2, 2)));
+    erode(ImageOut, ImageOut, erodeElement, Point(-1, -1), 2);
+    dilate(ImageOut, ImageOut, dilateElement, Point(-1, -1), 2);
 }
--- goal-detection/GoalDetector.hpp
+++ goal-detection/GoalDetector.hpp
@@ -7,37 +7,45 @@
 
 
 class GoalDetector {
-
 public:
 
-GoalDetector();
+    GoalDetector();
 
-float dist_to_goal()const { return _dist_to_goal; } //floor distance to goal in m
-float angle_to_goal()const { return _angle_to_goal; } //angle robot has to turn to face goal in degrees
+    float dist_to_goal() const { return _dist_to_goal; }   //floor distance to goal in m
+    float angle_to_goal() const { return _angle_to_goal; } //angle robot has to turn to face goal in degrees
 
-bool processFrame(cv::Mat &image, cv::Mat &depth); //this updates dist_to_goal and angle_to_goal
+    bool processFrame(cv::Mat& image, cv::Mat& depth); //this updates dist_to_goal and angle_to_goal
 
 private:
 
-std::vector< cv::Point2f > _goal_shape_contour; //hold the shape of the goal so we can easily get info from it
-float _camera_hfov = 84.14 * (M_PI / 180.0); //determined experimentally
-float _camera_vfov = 53.836 * (M_PI / 180.0); //determined experimentally
-float _goal_height = 2.159; //in m
+    std::vector<cv::Point2f> _goal_shape_contour; //hold the shape of the goal so we can easily get info from it
+    float _camera_hfov = 84.14 * (M_PI / 180.0);  //determined experimentally
+    float _camera_vfov = 53.836 * (M_PI / 180.0); //determined experimentally
+    float _goal_height = 2.159;                   //in m
 
-int _hue_min = 60; //60-95 is a good range for bright green
-int _hue_max = 95;
-int _sat_min =  180;
-int _sat_max = 255;
-int _val_min =  67;
-int _val_max = 255;
+    int _hue_min = 60;                            //60-95 is a good range for bright green
+    int _hue_max = 95;
+    int _sat_min = 180;
+    int _sat_max = 255;
+    int _val_min = 67;
+    int _val_max = 255;
 
-float _dist_to_goal;
-float _angle_to_goal;
+    float _dist_to_goal;
+    float _angle_to_goal;
 
-void generateThreshold(const cv::Mat &ImageIn, cv::Mat &ImageOut, int H_MIN, int H_MAX, int S_MIN, int S_MAX, int V_MIN, int V_MAX);
+    void generateThreshold(const cv::Mat& ImageIn, cv::Mat& ImageOut, int H_MIN, int H_MAX, int S_MIN, int S_MAX, int V_MIN, int V_MAX);
 
-float minOfMat(cv::Mat &img, cv::Mat &mask, bool (*f)(float), bool max=false, int range=10);
+    float minOfMat(cv::Mat& img, cv::Mat& mask, bool (*f)(float), bool max = false, int range = 10);
 
-static bool countPixel(float v) { if( isnan(v) || v <= 0) { return false; } else { return true; } } //small inline function to pass to minOfMat
-
+    static bool countPixel(float v)
+    {
+        if (isnan(v) || (v <= 0))
+        {
+            return false;
+        }
+        else
+        {
+            return true; 
+        }
+    }                                                                                               //small inline function to pass to minOfMat
 };
