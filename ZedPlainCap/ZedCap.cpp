97e26e00019be884c24ebe7234e648dff683fb4d
Kevin Jaget <kjaget@gmail.com>
2016-02-20 10:55:17 -0500

    Allow svo->zms conversion

--- ZedPlainCap/ZedCap.cpp
+++ ZedPlainCap/ZedCap.cpp
@@ -17,39 +17,49 @@ using namespace cv;
 
 int main(int argc, char *argv[])
 {
-    ZedIn camera(argc == 2 ? argv[1] : NULL);
-    //ZedIn camera(NULL, argc == 2 ? argv[1] : NULL);
+	ZedIn camera(argc >= 2 ? argv[1] : NULL, argc == 3 ? argv[2] : NULL);
+	//ZedIn camera(NULL, argc == 2 ? argv[1] : NULL);
 
-    char name[PATH_MAX];
-    int  index = 0;
-    int  rc;
-    struct stat statbuf;
-    do
-    {
-        sprintf(name, "cap%d.avi", index++);
-        rc = stat(name, &statbuf);
-    } while (rc == 0);
-    VideoWriter outputVideo(name, CV_FOURCC('M', 'J', 'P', 'G'), 30, Size(camera.width(), camera.height()), true);
-    Mat frame;
+	VideoWriter outputVideo;
+	if (argc < 3)
+	{
+		char name[PATH_MAX];
+		int  index = 0;
+		int  rc;
+		struct stat statbuf;
+		do
+		{
+			sprintf(name, "cap%d.avi", index++);
+			rc = stat(name, &statbuf);
+		} while (rc == 0);
+		outputVideo = VideoWriter(name, CV_FOURCC('M', 'J', 'P', 'G'), 30, Size(camera.width(), camera.height()), true);
+	}
+	Mat frame;
 
-    while (true)
-    {
-        camera.getNextFrame(frame, false);
-        if (!frame.empty())
-        {
-            outputVideo << frame;
-        }
-        else
-        {
-            fprintf(stderr, "Unable to grab frame.\n");
-            break;
-        }
-	imshow("Frame", frame);
-        uchar wait_key = cv::waitKey(2);
-        if ((wait_key == 27) || (wait_key == 32))
-        {
-            break;
-        }
-    }
-    fprintf(stdout, "Closing camera.\n");
+	while (true)
+	{
+		camera.getNextFrame(frame, false);
+		if (!frame.empty())
+		{
+			if (argc < 3)
+			{
+				outputVideo << frame;
+			}
+		}
+		else
+		{
+			fprintf(stderr, "Unable to grab frame.\n");
+			break;
+		}
+		if (argc < 3)
+		{
+			imshow("Frame", frame);
+			uchar wait_key = cv::waitKey(2);
+			if ((wait_key == 27) || (wait_key == 32))
+			{
+				break;
+			}
+		}
+	}
+	fprintf(stdout, "Closing camera.\n");
 }
