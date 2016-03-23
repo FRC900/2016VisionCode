da207db6b8426ca10e0f15feca07689d77243724
Team 900 Programmer <programmers@team900.org>
2016-03-23 18:21:33 -0400

    Fix scripts

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,47 +1,62 @@
 #install basic dependencies
+sudo apt-get update
 
-sudo apt-get install libeigen3-dev build-essentials git libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install libeigen3-dev build-essential gfortran git cmake libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-dev libhdf5-serial-dev protobuf-compiler liblmdb-dev vim-gtk libgflags-dev libgoogle-glog-dev libatlas-base-dev python-dev python-pip
 
 sudo apt-get install --no-install-recommends libboost-all-dev
-
-sudo apt-get install libatlas-base-dev python-dev
-
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev protobuf-compiler cmake libhdf5-dev libhdf5-serial-dev
-
+sudo apt-get install v4l-conf v4l-utils 
 
 #install caffe
-cd ~/
+cd
 
-sudo git clone https://github.com/BVLC/caffe.git
-
+git clone https://github.com/BVLC/caffe.git
 cd caffe
+mkdir build
+cd build
+cmake ..
 
-sudo cp Makefile.config.example Makefile.config
+make -j4 all
+make test
+make runtest
+make install
 
+# Install libsodium - this is a prereq for zeromq
+wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.8.tar.gz
+tar -zxvf libsodium-1.0.8.tar.gz
+cd libsodium-1.0.8
+./configure
+make -j4 
+sudo make install
+cd ..
+rm -rf libsodium-1.0.8*
 
-
-sudo make -j all
-sudo make test
-sudo make runtest
-
-
+# install zeromq
+cd
+wget http://download.zeromq.org/zeromq-4.1.4.tar.gz
+tar -xzvf zeromq-4.1.4.tar.gz
+cd zeromq-4.1.4
+./configure
+make -j4
+sudo make install
+cd ..
+rm -rf zeromq-4.1.4*
+cd /usr/local/include/
+sudo wget https://raw.githubusercontent.com/zeromq/cppzmq/master/zmq.hpp
 
 #Build repo
-
-cd ~/
-sudo git clone https://github.com/FRC900/2016VisionCode.git
-
+cd
+git clone https://github.com/FRC900/2016VisionCode.git
 cd 2016VisionCode
 
 cd libfovis
-sudo mkdir build
+mkdir build
 cd build
-sudo cmake ..
-sudo make -j
+cmake ..
+make -j4
 cd ../..
 cd bindetection
-sudo cmake .
-sudo make -j
+cmake .
+make -j4
 
-
-
+sudo mkdir /mnt/900_2
+sudo cp ~/2016VisionCode/zv.conf /etc/init
