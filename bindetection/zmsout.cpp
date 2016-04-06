db5aa6005bc218fa7c23a05fc8c0abf8df37b670
Kevin Jaget <kjaget@nc.rr.com>
2016-04-05 22:00:35 -0400

    More frames per file

--- bindetection/zmsout.cpp
+++ bindetection/zmsout.cpp
@@ -17,7 +17,7 @@ using namespace boost::filesystem;
 // method to serialize image and depth data to disk rather than
 // relying on Stereolab's SVO format.
 ZMSOut::ZMSOut(const char *outFile, int frameSkip) :
-	MediaOut(frameSkip,  50),
+	MediaOut(frameSkip, 150),
 	fileName_(outFile),
 	serializeOut_(NULL),
 	filtSBOut_(NULL),
