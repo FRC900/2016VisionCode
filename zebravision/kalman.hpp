f23ca4ab38db848f35b279535f281455a2bef658
Kevin Jaget <kjaget@gmail.com>
2016-07-29 19:53:44 -0400

    Try to make filter DT a better match to actual framerate

--- zebravision/kalman.hpp
+++ zebravision/kalman.hpp
@@ -7,7 +7,7 @@
 class TKalmanFilter
 {
 	public:
-		TKalmanFilter(cv::Point3f p, float dt = 0.2, float Accel_noise_mag = 0.5);
+		TKalmanFilter(cv::Point3f p, float dt = 0.05, float Accel_noise_mag = 0.5);
 		~TKalmanFilter();
 		cv::Point3f GetPrediction();
 		cv::Point3f Update(cv::Point3f p);
