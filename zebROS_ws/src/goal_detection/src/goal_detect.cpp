7fb70ab6df02cf0709b6d5a249e53b9eb084bbaf
Team900 Jetson TX1 <adithyabsk@gmail.com>
2017-01-05 21:38:33 -0500

    Revert goal_detect to working state

--- zebROS_ws/src/goal_detection/src/goal_detect.cpp
+++ zebROS_ws/src/goal_detection/src/goal_detect.cpp
@@ -14,94 +14,11 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Float64MultiArray.h"
 
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
 #include <sstream>
 
 
 using namespace cv;
 using namespace std;
-
-namespace cv_bridge {
-
-class CvImage
-{
-public:
-  std_msgs::Header header;
-  std::string encoding;
-  cv::Mat image;
-};
-
-typedef boost::shared_ptr<CvImage> CvImagePtr;
-typedef boost::shared_ptr<CvImage const> CvImageConstPtr;
-
-}
-
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-	Mat image;
-	Mat depth;
-	//Mat depthNorm;
-	Rect bound;
-	FrameTicker frameTicker;
-GoalDetector* gd(Point2f(cap->getCameraParams().fov.x, 
-				            cap->getCameraParams().fov.y), 
-			        Size(cap->width(),cap->height()), true);
-  
-public:
-  ImageConverter()
-    : it_(nh_)
-  {
-	gd = new GoalDetector(Point2f(cap->getCameraParams().fov.x, 
-				            cap->getCameraParams().fov.y), 
-			        Size(cap->width(),cap->height()), true);
-
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/image_raw", 1, 
-      &ImageConverter::imageCb, this);
-    image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-    cv::namedWindow(OPENCV_WINDOW);
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-    
-    // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
-};
 
 int main(int argc, char **argv)
 {
@@ -117,16 +34,9 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	image_transport::ImageTransport it(nh);
-
-
-	cv_bridge::CvImagePtr cv_ptr;
-	try
-	{
-		cv_ptr = cv_bridge::toCvCopy()
-	}
-
-	
+	GoalDetector gd(Point2f(cap->getCameraParams().fov.x, 
+				            cap->getCameraParams().fov.y), 
+			        Size(cap->width(),cap->height()), true);
 
 	// zmq::context_t context(1);
 	// zmq::socket_t publisher(context, ZMQ_PUB);
@@ -140,8 +50,12 @@ int main(int argc, char **argv)
 	// std::cout<< "Starting network publisher 5800" << std::endl;
 	// publisher.bind("tcp://*:5800");
 
-	
-	while (ros::ok())
+	Mat image;
+	Mat depth;
+	//Mat depthNorm;
+	Rect bound;
+	FrameTicker frameTicker;
+	while (cap->getFrame(image, depth) && ros::ok())
 	{
 		frameTicker.mark();
 		//imshow ("Normalized Depth", depthNorm);
